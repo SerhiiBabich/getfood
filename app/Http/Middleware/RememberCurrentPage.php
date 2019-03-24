71fee3d8488c24bc851e2729a28e25c68bd2a0eb
Serhii Babich <sergeghost@gmail.com>
2019-03-24 22:31:28 +0200

    Start state

---
+++ app/Http/Middleware/RememberCurrentPage.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RememberCurrentPage
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+}
